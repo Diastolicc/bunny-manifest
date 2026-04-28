057c3f74c0029410e9b2f1e32e1b219d9b2c4956
cbalderama <devbalderama@gmail.com>
2026-04-29 05:28:16 +0800

    feat: Add Firebase mocking for tests

---
+++ test/firebase_mock.dart
@@ -0,0 +1,104 @@
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+typedef Callback = void Function(MethodCall call);
+
+void setupFirebaseMocks([Callback? customHandlers]) {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setupFirebaseCoreMocks();
+}
+
+Future<T> neverEndingFuture<T>() async {
+  // This is to prevent the test from finishing before the tester.pumpWidget completes
+  // See https://github.com/flutter/flutter/issues/88765
+  await Future.delayed(const Duration(days: 365));
+  throw '';
+}
+
+void setupFirebaseCoreMocks() {
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel(
+      'plugins.flutter.io/firebase_core',
+    ),
+    (methodCall) async {
+      if (methodCall.method == 'Firebase#initializeCore') {
+        return [
+          {
+            'name': '[DEFAULT]',
+            'options': {
+              'apiKey': 'test-api-key',
+              'appId': '1:123456789:web:abcdef',
+              'messagingSenderId': '123456789',
+              'projectId': 'test-project',
+            },
+            'pluginConstants': {},
+          }
+        ];
+      }
+
+      if (methodCall.method == 'Firebase#initializeApp') {
+        return {
+          'name': methodCall.arguments['appName'],
+          'options': methodCall.arguments['options'],
+          'pluginConstants': {},
+        };
+      }
+
+      return null;
+    },
+  );
+}
+
+void setupFirebaseAuthMocks() {
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel('plugins.flutter.io/firebase_auth'),
+    (MethodCall methodCall) async {
+      switch (methodCall.method) {
+        case 'Auth#registerIdTokenListener':
+          return {
+            'user': null,
+          };
+        case 'Auth#registerChangeListener':
+          return {
+            'user': null,
+          };
+        default:
+          return null;
+      }
+    },
+  );
+}
+
+void setupFirebaseFirestoreMocks() {
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel('plugins.flutter.io/cloud_firestore'),
+    (MethodCall methodCall) async {
+      return null;
+    },
+  );
+}
+
+void setupFirebaseStorageMocks() {
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel('plugins.flutter.io/firebase_storage'),
+    (MethodCall methodCall) async {
+      return null;
+    },
+  );
+}
+
+void setupFirebaseAnalyticsMocks() {
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(
+    const MethodChannel('plugins.flutter.io/firebase_analytics'),
+    (MethodCall methodCall) async {
+      return null;
+    },
+  );
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:bunny/main.dart';
+import 'firebase_mock.dart'; // ADD THIS LINE - Import the Firebase mocks
 
 void main() {
+  // ADD THIS BLOCK - Setup Firebase mocks before all tests run
+  setUpAll(() async {
+    setupFirebaseMocks();
+    setupFirebaseAuthMocks();
+    setupFirebaseFirestoreMocks();
+    setupFirebaseStorageMocks();
+    setupFirebaseAnalyticsMocks();
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
